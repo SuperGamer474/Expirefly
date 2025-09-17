fa3c68b03f2f54362ec91d3da218830e89e1c2dc
SuperGamer474 <223823408+SuperGamer474@users.noreply.github.com>
2025-09-17 17:44:52 +0930

    Update private var to var in expirefly.swift

--- Sources/expirefly/expirefly.swift
+++ Sources/expirefly/expirefly.swift
@@ -10,7 +10,7 @@ struct ExpireflyApp: App {
 }
 
 struct ContentView: View {
-    @State private var items: [String] = ["Milk", "Eggs", "Cheese"]
+    @State var items: [String] = ["Milk", "Eggs", "Cheese"]
 
     var body: some View {
         NavigationStack {
